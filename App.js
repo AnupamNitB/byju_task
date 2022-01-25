77965e8a13da1d33f8ea3a3b5c3882393617615e
AnupamNitB <legendanupam@gmail.com>
2022-01-25 11:46:44 +0530

    curd operation

---
+++ App.js
@@ -0,0 +1,38 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import NoteScreen from "./app/screens/NoteScreen";
+import NoteDetail from "./app/components/NoteDetail";
+import NoteProvider from "./app/contexts/NoteProvider";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  const renderNoteScreen = (props) => <NoteScreen {...props} />;
+
+  return (
+    <NavigationContainer>
+      <NoteProvider>
+        <Stack.Navigator
+          screenOptions={{ headerTitle: "", headerTransparent: true }}
+        >
+          <Stack.Screen component={renderNoteScreen} name="NoteScreen" />
+          <Stack.Screen component={NoteDetail} name="NoteDetail" />
+        </Stack.Navigator>
+      </NoteProvider>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ app/screens/NoteScreen.js
@@ -0,0 +1,120 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  StatusBar,
+  TouchableWithoutFeedback,
+  Keyboard,
+  FlatList,
+} from "react-native";
+import Note from "../components/Note";
+import NoteInputModal from "../components/NoteInputModal";
+import RoundIconBtn from "../components/RoundIconBtn";
+import { useNotes } from "../contexts/NoteProvider";
+import colors from "../misc/colors";
+
+const reverseData = (data) => {
+  return data.sort((a, b) => {
+    const aInt = parseInt(a.time);
+    const bInt = parseInt(b.time);
+    if (aInt < bInt) return 1;
+    if (aInt == bInt) return 0;
+    if (aInt > bInt) return -1;
+  });
+};
+
+const NoteScreen = ({ user, navigation }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const { notes, setNotes } = useNotes();
+
+  const reverseNotes = reverseData(notes);
+
+  const handleOnSubmit = async (title, desc) => {
+    const note = { id: Date.now(), title, desc, time: Date.now() };
+    const updatedNotes = [...notes, note];
+    setNotes(updatedNotes);
+    await AsyncStorage.setItem("notes", JSON.stringify(updatedNotes));
+  };
+
+  const openNote = (note) => {
+    navigation.navigate("NoteDetail", { note });
+  };
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor={colors.LIGHT} />
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <FlatList
+            data={reverseNotes}
+            numColumns={2}
+            columnWrapperStyle={{
+              justifyContent: "space-between",
+              marginBottom: 15,
+            }}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <Note onPress={() => openNote(item)} item={item} />
+            )}
+          />
+
+          {!notes.length ? (
+            <View
+              style={[
+                StyleSheet.absoluteFillObject,
+                styles.emptyHeaderContainer,
+              ]}
+            >
+              <Text style={styles.emptyHeader}>Add Notes</Text>
+            </View>
+          ) : null}
+        </View>
+      </TouchableWithoutFeedback>
+      <RoundIconBtn
+        onPress={() => setModalVisible(true)}
+        antIconName="plus"
+        style={styles.addBtn}
+      />
+      <NoteInputModal
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+        onSubmit={handleOnSubmit}
+      />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  container: {
+    paddingHorizontal: 20,
+    flex: 1,
+    zIndex: 1,
+    marginVertical: 40,
+  },
+  emptyHeader: {
+    fontSize: 30,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    opacity: 0.2,
+  },
+  emptyHeaderContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: -1,
+  },
+  addBtn: {
+    position: "absolute",
+    right: 15,
+    bottom: 50,
+    zIndex: 1,
+  },
+});
+
+export default NoteScreen;
